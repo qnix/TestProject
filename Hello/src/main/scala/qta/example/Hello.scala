ea548fe8361e6f2c28ff9e0a4cc00af2c8642ca3
Quan Ta <qta@paxata.com>
2016-06-08 20:12:43 -0700

    feature-one: add Bar

--- Hello/src/main/scala/qta/example/Hello.scala
+++ Hello/src/main/scala/qta/example/Hello.scala
@@ -4,10 +4,18 @@ case class Foo(value: String) {
   println(s"Foo with value: ${value}")
 }
 
+case class Bar() {
+  println("this is Bar")
+}
+
 object Hello {
   def main(args: Array[String]) {
     println(">>>> Hello")
 
     val f = Foo("Test Foo")
+    val b = Bar()
+
+    val c = Bar
+    val x = c()
   }
 }
